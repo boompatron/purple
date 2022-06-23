f163b5b9bf9c725d0295720cd5f56725e6aa392c
Bosub Kim <39071638+boompatron@users.noreply.github.com>
2022-06-23 22:48:05 +0900

    현재 구글 로그인 관련 문제 해결 시도 중... 어떻게 SessionUSer가 User로 캐스팅 될 수 있는지 이해 시도중 이 외에 방법으로 시도해보고... 내일 해봐야지 일단 여기까지 오는 것도 굉장히 이해하기 어려웠음 구글 로그인을 넣기 위해 OAuth 관련 config 클래스와 다양한 클래스 추가함 config 클래스 이해하는게 제일 어려움

--- src/main/java/org/example/springboot/web/IndexController.java
+++ src/main/java/org/example/springboot/web/IndexController.java
@@ -1,6 +1,8 @@
 package org.example.springboot.web;
 
 import lombok.RequiredArgsConstructor;
+import org.example.springboot.config.auth.dto.SessionUser;
+import org.example.springboot.domain.user.User;
 import org.example.springboot.service.PostsService;
 import org.example.springboot.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
@@ -8,15 +10,32 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (User) httpSession.getAttribute("user");
+        // SessionUser user = new SessionUser((User)httpSession.getAttribute("user"));
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
+//    @GetMapping("/")
+//    public String index(Model model, @LoginUser SessionUser user) {
+//        model.addAttribute("posts", postsService.findAllDesc());
+//        if (user != null) {
+//            model.addAttribute("userName", user.getName());
+//        }
+//        return "index";
+//    }
 
     @GetMapping("/posts/save")
     public String postsSave(){
