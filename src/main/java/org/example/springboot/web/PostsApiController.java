3f75cc4bc346c01100f564afd3cea302f9d64fbd
Bosub Kim <39071638+boompatron@users.noreply.github.com>
2022-06-26 01:30:46 +0900

    제발.......요

--- src/main/java/org/example/springboot/web/PostsApiController.java
+++ src/main/java/org/example/springboot/web/PostsApiController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 public class PostsApiController {
     private final PostsService postsService;
 
+
+
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestsDto requestsDto){
         return postsService.save(requestsDto);
